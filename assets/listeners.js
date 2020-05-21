ae237f3fb9020e3b32ac6fc1475ebad48643a645
Bogdan <36476272+Avatar87@users.noreply.github.com>
2020-05-21 19:02:00 +0300

    Add files via upload

---
+++ assets/listeners.js
@@ -0,0 +1,98 @@
+window.onload = () => {
+    document.getElementById('from').addEventListener('change', () => {
+        document.getElementById('loader').style.display='block';
+        const data = {fromdate: document.getElementById('from').value};
+        const url = 'http://'+window.location.host+'/date';
+        fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+              //'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        })
+        .then(response => {  
+            if (response.status !== 200) {  
+              console.log('Looks like there was a problem. Status Code: ' +  
+                response.status);  
+              return;  
+            }
+            response.json().then(function(data) {  
+              if(data.response == 'success'){
+                document.location.reload();
+                document.getElementById('loader').style.display='none';
+              }
+            });
+        });        
+    })
+
+    document.getElementById('to').addEventListener('change', () => {
+      document.getElementById('loader').style.display='block';
+      const data = {todate: document.getElementById('to').value};
+      const url = 'http://'+window.location.host+'/date';
+      fetch(url, {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json'
+            //'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow',
+          referrerPolicy: 'no-referrer',
+          body: JSON.stringify(data)
+      })
+      .then(response => {  
+          if (response.status !== 200) {  
+            console.log('Looks like there was a problem. Status Code: ' +  
+              response.status);  
+            return;  
+          }
+          response.json().then(function(data) {  
+            if(data.response == 'success'){
+              document.location.reload();
+              document.getElementById('loader').style.display='none';
+            }
+          });
+      });
+    })
+
+  document.getElementById('all').addEventListener('change', () => {
+    document.getElementById('loader').style.display='block';
+    var data;
+    if(document.getElementById('all').checked){
+      data = {displayall: document.getElementById('all').value};
+    }
+    else{
+      data = {fromdate: document.getElementById('from').value, todate: document.getElementById('to').value};
+    }
+    const url = 'http://'+window.location.host+'/date';
+    fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+          //'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    })
+    .then(response => {  
+        if (response.status !== 200) {  
+          console.log('Looks like there was a problem. Status Code: ' +  
+            response.status);  
+          return;  
+        }
+        response.json().then(function(data) {  
+          if(data.response == 'success'){
+            document.location.reload();
+            document.getElementById('loader').style.display='none';
+          }
+        });
+    });
+  })
+
+}
